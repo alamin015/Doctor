3fffb3002bebd814b4767177c15a5265c90100f9
Alamin <alaminsf40@gmail.com>
2021-12-26 15:30:19 +0600

    navbar okay

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ AOS.init({
     duration: 1000,
     // offset: 400,
 });
+
+// sticky navbar set 
+
+
+
+window.addEventListener("scroll",() => {
+
+    const navbar  = document.getElementById("header")
+    navbar.classList.toggle("active",window.scrollY > 100)
+})
