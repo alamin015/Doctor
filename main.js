f4b2b0d8d19b2332b48382038bdf99347632406c
Alamin <alaminsf40@gmail.com>
2021-12-27 19:28:13 +0600

    carousel

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
+// carousel slide call 
 
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop : true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        nav : true,
+        autoplayHoverPause:true,
+        // responsiveClass:true,
+        responsive:{
+        0:{
+            items:1,
+            nav:true,
+            loop : true,
+        },
+        600:{
+            items:2,
+            nav:true,
+            loop : true,
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:true
+        }
+    }
+    });
+  });
 
 AOS.init({
     duration: 1000,
